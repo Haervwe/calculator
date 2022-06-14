862c056723a14221d7b030018cc15c649c801e55
Haervwe <juan-jose-007@hotmail.com>
2022-06-14 03:36:40 -0300

    add parethesis support

--- scrypt.js
+++ scrypt.js
@@ -47,52 +47,97 @@ function sqrt (a){
 
 //function that takes a string and caculates a result, inputs must have a space between 
 //acomplished by giving operation inputs the structure " "operator" " .
+//recursive parentesis implementation.
 
 function calculate (expresion) {
-    let result = 0;
-    const separated = expresion.split(" ");
     
-    for(let i=0; i<separated.length;i++){
-        
-        if (separated[i]=="s"){
-            separated.splice(i,2, sqrt(separated[i+1]));
+    let separated = [];
+    if (typeof(expresion)== "string"){
+        separated = expresion.split(" ");
+    } else {
+        separated = expresion;
+    }
+    let counter = 0;
+    let check = 0;
+    loop:for(let i=0; i<separated.length;i++){
+        if (separated[i]=="("){
+            let init = i;
+            check = 1;
+            for(let j = i; j<separated.length;j++){
+                if (separated[j]=="("){
+                    counter++;
+                }
+                if (separated[j]==")" && counter > 0){
+                    counter--;
+                }
+                if (separated[j]==")" && counter == 0){
+                    let fin = j-init;
+                    const temp = separated;
+                    const toCalc = separated.splice(init,fin);
+                    toCalc.shift();
+                    console.log(toCalc);
+                    console.log(temp);
+                    temp.splice(init,1,calculate(toCalc));
+                    console.log(temp[init]);
+                    temp[init]=temp[init][0]
+                    console.log(temp);
+                    separated  = temp;
+                    break;
+                    
+                }
+            }
+        }
+    }
+
+    for (let i = 0 ; i<separated.length;i++){
+        if (separated[i]=="("){
+            check = 1;
+            console.log(check);
+        }
+        else{
+            check = 0 ;
         }
     }
     
-    for(let i=0; i<separated.length;i++){
+    if (!check){
+        console.log(separated);
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="s"){
+                separated.splice(i,2, sqrt(separated[i+1]));
+            }
+        }
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="^"){
+                separated.splice(i-1,3, power(separated[i-1],separated[i+1]));
+            }
+        }
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="x"){
+                separated.splice(i-1,3, multiply(separated[i-1],separated[i+1]));
+            }
+        }
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="/"){
+                separated.splice(i-1,3, divide(separated[i-1],separated[i+1]));
+            }
+        }
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="+"){
+                separated.splice(i-1,3, add(separated[i-1],separated[i+1]));
+            }
+        }
+        for(let i=0; i<separated.length;i++){
+            if (separated[i]=="-"){
+                separated.splice(i-1,3, substract(separated[i-1],separated[i+1]));
+            }
+        }
         
-        if (separated[i]=="^"){
-            separated.splice(i-1,3, power(separated[i-1],separated[i+1]));
-        }
-    }
-    for(let i=0; i<separated.length;i++){
-        
-        if (separated[i]=="x"){
-            separated.splice(i-1,3, multiply(separated[i-1],separated[i+1]));
-        }
-    }
-    for(let i=0; i<separated.length;i++){
-        
-        if (separated[i]=="/"){
-            separated.splice(i-1,3, divide(separated[i-1],separated[i+1]));
-        }
-    }
-    for(let i=0; i<separated.length;i++){
-        console.log(separated[i]);
-        if (separated[i]=="+"){
-            separated.splice(i-1,3, add(separated[i-1],separated[i+1]));
-        }
-    }
-    for(let i=0; i<separated.length;i++){
-        console.log(separated[i]);
-        if (separated[i]=="-"){
-            separated.splice(i-1,3, substract(separated[i-1],separated[i+1]));
-        }
     }
     return separated;
 }
 
-console.log(calculate("10 x 10 + 5 / s 4 ^ 2 "));
+console.log(calculate("10 x ( 10 + 5 ) / s ( 4 ^ 2 )"));
+
 
 
 
